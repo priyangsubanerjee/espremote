a16134587d770b3810f2e15dd4e48f15f96d734f
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-01-14 02:18:17 +0530

    ui

--- helpers/firebaseApp.js
+++ helpers/firebaseApp.js
@@ -2,16 +2,16 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
-    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
-    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-    messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
-    appId: process.env.NEXT_PUBLIC_APP_ID,
-    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
-const firebaseApp = initializeApp(firebaseConfig)
+const firebaseApp = initializeApp(firebaseConfig);
 
 export default firebaseApp;
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,62 +1,66 @@
 /* eslint-disable @next/next/no-img-element */
-import Head from 'next/head'
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+import styles from "../styles/Home.module.css";
 import { getDatabase, ref, set, onValue } from "firebase/database";
-import firebaseApp from '../helpers/firebaseApp';
-import Switch from '@mui/material/Switch';
-
+import firebaseApp from "../helpers/firebaseApp";
+import Switch from "@mui/material/Switch";
 
 export default function Home() {
-
-  const [database, setDatabase] = useState(null)
+  const [database, setDatabase] = useState(null);
   const [ledState, setLedState] = useState(null);
   const [connected, setConnected] = useState(false);
 
-  const label = { inputProps: { 'aria-label': 'Switch demo' } };
+  const label = { inputProps: { "aria-label": "Switch demo" } };
 
   useEffect(() => {
     (async () => {
-      const database_ = getDatabase(firebaseApp)
-      setDatabase(database_)
-    })()
-  }, [])
+      const database_ = getDatabase(firebaseApp);
+      setDatabase(database_);
+    })();
+  }, []);
 
   useEffect(() => {
     if (database) {
-      const starCountRef = ref(database, '/board/14');
+      const starCountRef = ref(database, "/board/14");
       onValue(starCountRef, (snapshot) => {
         const data = snapshot.val();
         setLedState(data);
         setConnected(true);
       });
     }
-  }, [database])
+  }, [database]);
 
   const handleLedState = (state) => {
-    const starCountRef = ref(database, '/board/14');
+    const starCountRef = ref(database, "/board/14");
     set(starCountRef, state);
     setLedState(state);
-  }
-
+  };
 
   return (
-    <div>
-
-      {
-        connected ?
-
-          <div className='p-10 flex items-center'>
-            <img src="/led_green.png" className='h-20 mr-10' alt="" />
-            <Switch checked={ledState == 1} onChange={() => {
-              handleLedState(ledState == 1 ? 0 : 1)
-            }} {...label} />
+    <div className="h-screen">
+      {connected ? (
+        <div className="p-10">
+          <h1 className="text-3xl text-slate-800 font-medium">
+            Led state:{" "}
+            <span className="text-blue-500">
+              {ledState == 1 ? "ON" : "OFF"}
+            </span>
+          </h1>
+          <div className="flex h-36 w-36 border items-center justify-center bg-neutral-50 rounded-lg mt-7">
+            <Switch
+              checked={ledState == 1}
+              onChange={() => {
+                handleLedState(ledState == 1 ? 0 : 1);
+              }}
+              {...label}
+            />
           </div>
-          :
-          <h1>Loading...</h1>
-      }
-
+        </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </div>
-  )
+  );
 }
