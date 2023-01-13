6980fb111754c3ab66383b3e4c9c146997d835e9
priyangsubanerjee <priyangsu26@gmail.com>
2023-01-14 01:12:12 +0530

    init

--- helpers/firebaseApp.js
+++ helpers/firebaseApp.js
@@ -2,14 +2,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCpeJy5LrePztFONmb4Yqt6lrJ7QWYfc98",
-    authDomain: "esp8266-a2d9a.firebaseapp.com",
-    databaseURL: "https://esp8266-a2d9a-default-rtdb.firebaseio.com",
-    projectId: "esp8266-a2d9a",
-    storageBucket: "esp8266-a2d9a.appspot.com",
-    messagingSenderId: "143649723819",
-    appId: "1:143649723819:web:d056d7f27f3e480ba06eee",
-    measurementId: "G-FR6BBYSDX8"
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    databaseURL: process.env.NEXT_PUBLIC_DATABASE_URL,
+    projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
+    measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig)
