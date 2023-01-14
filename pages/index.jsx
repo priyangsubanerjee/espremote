46e8f2cfaace1db8bf9cd59afd0779b272f28127
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-01-14 17:47:28 +0530

    form

--- pages/index.jsx
+++ pages/index.jsx
@@ -5,33 +5,42 @@ import React, { useState, useEffect } from "react";
 export default function Home() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/home");
+    //router.push("/home");
   }, []);
   return (
-    <div className="h-screen w-screen bg-white">
-      <div className="relative">
-        <div className="p-5  bottom-0">
-          <h1 className="text-3xl font-bold text-slate-900 mt-5">
-            Login to Home control
-          </h1>
-          <p className="text-xs text-neutral-500 leading-5 mt-2">
-            Take control of your home with this app.
-          </p>
-        </div>
-      </div>
+    <div className="h-screen w-screen bg-neutral-50 overflow-auto">
+      <div className="p-5 bg-transparent mt-10 flex flex-col items-center justify-center">
+        <img
+          src="https://cdn-icons-png.flaticon.com/512/9346/9346603.png"
+          alt=""
+          className="h-20 w-20"
+        />
+        <h1 className="text-2xl font-bold mt-8">Welcome to Home Control</h1>
+        <p className="text-neutral-500 mt-2 text-sm leading-6">
+          Take control of your home from anywhere.
+        </p>
 
-      <div className="p-5">
-        <div className="space-y-4">
-          <TextField type={"email"} fullWidth label="Enter your email" />
-          <TextField
-            type={"password"}
-            fullWidth
-            label="Enter account password"
-          />
-        </div>
-        <button className="bg-slate-900 w-full h-14 mt-7 rounded-full text-white font-semibold">
-          Login
-        </button>
+        <form action="">
+          <div className="mt-10 w-full bg-white rounded-xl border overflow-hidden">
+            <input
+              type="email"
+              placeholder="Enter your registered email"
+              className="p-4 border-b w-full"
+              name=""
+              id=""
+            />
+            <input
+              type="password"
+              placeholder="Enter your account password"
+              className="p-4 border-b-0 w-full"
+              name=""
+              id=""
+            />
+          </div>
+          <button className="w-full mt-5 text-center p-3 active:bg-blue-600 text-white rounded-xl bg-blue-500">
+            Submit
+          </button>
+        </form>
       </div>
     </div>
   );
