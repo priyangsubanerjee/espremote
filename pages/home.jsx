2737e1cd56872e7a2021e18dff7211b9553902e9
Priyangsu Banerjee <priyangsu26@gmail.com>
2023-01-14 18:20:19 +0530

    ui

--- pages/home.jsx
+++ pages/home.jsx
@@ -1,3 +1,4 @@
+import { Switch } from "@mui/material";
 import { getDatabase, ref, set, onValue } from "firebase/database";
 import React, { useEffect, useState } from "react";
 import firebaseApp from "../helpers/firebaseApp";
@@ -77,58 +78,45 @@ function Home() {
               <p className="text-xs text-neutral-500 leading-5 mt-2">
                 Take control of your home with this app.
               </p>
-              <div className="mt-10 grid grid-cols-2 lg:grid-cols-4 gap-4">
+              <div className="mt-10 grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4">
                 <button
                   onClick={() => handleLedState(ledState == 1 ? 0 : 1)}
                   className={`${
-                    ledState == 1 ? "bg-green-500" : "bg-white"
-                  } shadow-lg active:shadow-none shadow-slate-100 rounded-lg flex items-center p-5 transition-all`}
+                    ledState == 1
+                      ? "bg-green-500 shadow-lg active:shadow-none shadow-green-200"
+                      : "bg-white shadow-lg active:shadow-none shadow-slate-100"
+                  } s rounded-lg flex flex-col space-y-4 items-center py-4 transition-all`}
                 >
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke-width="1.5"
-                    stroke="currentColor"
-                    class={`w-7 h-7 ${
-                      ledState == 1 ? "text-white" : "text-sky-500"
+                  <i
+                    class={`bi bi-lightbulb text-3xl ${
+                      ledState == 1 ? "text-white" : "text-green-500"
                     }`}
-                  >
-                    <path
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
-                      d="M12 18v-5.25m0 0a6.01 6.01 0 001.5-.189m-1.5.189a6.01 6.01 0 01-1.5-.189m3.75 7.478a12.06 12.06 0 01-4.5 0m3.75 2.383a14.406 14.406 0 01-3 0M14.25 18v-.192c0-.983.658-1.823 1.508-2.316a7.5 7.5 0 10-7.517 0c.85.493 1.509 1.333 1.509 2.316V18"
-                    />
-                  </svg>
-                  <div className="text-left ml-4">
-                    <h1
-                      className={`${
-                        ledState == 1 ? "text-white" : "text-neutral-700"
-                      } font-semibold text-lg`}
-                    >
-                      Led green
-                    </h1>
+                  ></i>
+                  <div className="text-left">
                     <p
                       className={`${
-                        ledState == 1 ? "text-neutral-100" : "text-neutral-500"
-                      } text-xs mt-1`}
+                        ledState == 1 ? "text-white" : "text-neutral-600"
+                      } text-sm font-medium`}
                     >
-                      Connected
+                      Led Green
                     </p>
                   </div>
                 </button>
-                <button className="bg-white shadow-lg shadow-slate-100 rounded-lg flex items-center p-5">
-                  <img
-                    src="https://cdn-icons-png.flaticon.com/512/8101/8101858.png"
-                    className="h-7 w-7"
-                    alt=""
-                  />
+                <button
+                  className={`${
+                    false == 1
+                      ? "bg-sky-500 shadow-lg active:shadow-none shadow-sky-200"
+                      : "bg-white shadow-lg active:shadow-none shadow-slate-100"
+                  } s rounded-lg flex flex-col space-y-4 items-center py-4 transition-all`}
+                >
+                  <i class="bi bi-fan text-3xl text-green-500"></i>
                   <div className="text-left">
-                    <h1 className="text-neutral-700 font-semibold text-lg ml-5">
+                    <p
+                      className={`${
+                        false == 1 ? "text-white" : "text-neutral-600"
+                      } text-sm font-medium`}
+                    >
                       Fan
-                    </h1>
-                    <p className="text-neutral-500 text-xs ml-5 mt-1">
-                      Connected
                     </p>
                   </div>
                 </button>
